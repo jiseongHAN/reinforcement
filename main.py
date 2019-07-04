80a7d204852343ef0e9a63be1653aeb4341b98f1
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-04 11:36:21 +0900

    Update main.py
    add reward clip

--- main.py
+++ main.py
@@ -6,37 +6,51 @@ import gym
 from collections import deque
 import hp
 import numpy as np
+import math
+env = gym.make('LunarLanderContinuous-v2')
+# env = gym.make('MountainCarContinuous-v0')
 
-env = gym.make('BipedalWalker-v2')
+
 action_dim = env.action_space.shape[0]
 input_dim = env.observation_space.shape[0]
 actor = Actor(input_dim,action_dim)
+buffer = []
 critic = Critic(input_dim)
-buffer = deque(maxlen=10000)
 actor_optim = optim.Adam(actor.parameters(), lr=hp.actor_lr)
 critic_optim = optim.Adam(critic.parameters(), lr=hp.critic_lr,
                           weight_decay=hp.l2_rate)
 n_train = 0
+loss_lst = []
 for _ in range(hp.iter_max):
     s = env.reset()
     done = False
     score = 0.0
     step =0
     while not done:
-        # env.render()
+        env.render()
+        # s = norm_state(s)
         mu, std, logstd = actor(torch.from_numpy(s).float())
         a = get_action(mu,std)
         s_prime, r, done, _ = env.step(torch.clamp(torch.Tensor(a),-1,1).data.numpy())
         score += r
-        r = np.clip(r,-1,1)
+        r = np.clip(r,-5,5)
         done_mask = 0 if done else 1
+        # s_prime = norm_state(s_prime)
         sarsd = (s,a,r,s_prime, done_mask)
         buffer.append(sarsd)
         step += 1
+        s = s_prime
         if done:
             break
-    train(actor,critic,buffer,actor_optim,critic_optim)
-    n_train += 1
+    if len(buffer) > 2000:
+        loss = train(actor,critic,buffer,actor_optim,critic_optim)
+        loss_lst.append(loss)
+        n_train += 1
+        buffer = []
+    if score > 200.0:
+        print('Clear!!')
+        break
     print('#{} : 총 점수 : {} 총 스텝 : {}'.format(n_train,score,step))
     score = 0.0
     step = 0
+env.close()
