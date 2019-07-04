715be908fd9f81aa483389aebc3fbcc78a83dce9
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-04 11:34:48 +0900

    Update hp.py

--- hp.py
+++ hp.py
@@ -1,10 +1,10 @@
 hidden = 256
 gamma = 0.99
-lam = 0.95
-eps = 0.1
+lam = 0.98
+eps = 0.2
 epoch = 10
 iter_max = 10000
-actor_lr = 0.0001
-critic_lr = 0.0001
+actor_lr = 0.0002
+critic_lr = 0.0002
 l2_rate = 0.001
 batch_size = 64
