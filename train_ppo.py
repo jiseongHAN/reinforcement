2ab9443e9a8fe7db51b144788d6cfc93c35f844d
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-04 11:43:13 +0900

    fix adv, optim update method, check matrix dim
    adv 구할 때 done_mask 업데이트
    matrix dimension 확인
    파라마티 업데이트 할 때 액터 따로 크리틱 따로 하는 것

--- train_ppo.py
+++ train_ppo.py
@@ -5,14 +5,18 @@ import torch
 from collections import deque
 import hp
 import random
+import numpy as np
+from tqdm import tqdm
 
 ##s a r s' done
 
-def train(actor, critic, data, actor_optim, critic_optim):
+def train(actor, critic, buffer, actor_optim, critic_optim):
+    mse = nn.MSELoss()
     s_lst, a_lst, r_lst, s_prime_lst, done_lst = [],[],[],[],[]
-    random.shuffle(data)
-    data = list(data)
-    for transition in data:
+    idx = np.arange(len(buffer))
+    random.shuffle(idx)
+    loss_lst = []
+    for transition in buffer:
         s, a, r, s_prime, done = transition
         s_lst.append(s)
         a_lst.append(a)
@@ -25,40 +29,50 @@ def train(actor, critic, data, actor_optim, critic_optim):
     r = torch.tensor(r_lst, dtype=torch.float)
     s_prime = torch.tensor(s_prime_lst, dtype=torch.float)
     done = torch.tensor(done_lst, dtype=torch.float)
-
     mu, std, logstd = actor(s)
-    old_p = log_density(a, mu, std, logstd)
-
-    for n in range(len(data) // hp.batch_size):
-        s=s[n*hp.batch_size:(n+1)*hp.batch_size]
-        a=a[n*hp.batch_size:(n+1)*hp.batch_size]
-        r=r[n*hp.batch_size:(n+1)*hp.batch_size]
-        s_prime=s_prime[n*hp.batch_size:(n+1)*hp.batch_size]
-        done=done[n*hp.batch_size:(n+1)*hp.batch_size]
-
+    old_v =  critic(s)
+    old_p = log_density(a, mu, std, logstd).detach()
+    td_target = r + hp.gamma * critic(s_prime) * done
+    delta = td_target - old_v
+    delta_d = delta * done
+    adv = discounted_r(delta_d, gamma=hp.gamma, gae=True)
+    adv = torch.tensor(adv)
+    adv = (adv - adv.mean()) / (adv.std() + 1e-12)
+    # returns = discounted_r(r,gamma=hp.gamma,gae=False)
+    # returns = torch.tensor(returns).unsqueeze(-1)
+    for n in tqdm(range(len(buffer) // hp.batch_size)):
+        s_b=s[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
+        a_b=a[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
+        old_b = old_p[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
+        td_b = td_target[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
+        adv_b = adv[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
+        # ret_b = returns[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
+        # old_v_b = old_v[idx[n*hp.batch_size:(n+1)*hp.batch_size]]
         for _ in range(hp.epoch):
-            td_target = r + hp.gamma * critic(s_prime) * done
-            delta = td_target - critic(s)
-            adv = discounted_r(delta, gamma=hp.gamma, gae=True,lam=hp.lam)
-            adv = torch.tensor(adv)
-            adv = (adv - adv.mean()) / (adv.std() + 1e-12)
-            new_mu, new_std, new_log = actor(s)
-            new_p = log_density(a,new_mu,new_std,new_log)
-            ratio = new_p / old_p
-            surr = ratio * adv
+            new_v = critic(s_b)
+            new_mu, new_std, new_log = actor(s_b)
+            new_p = log_density(a_b,new_mu,new_std,new_log)
+            ratio = torch.exp(new_p - old_b)
+            surr = ratio.squeeze() * adv_b
+            clip = torch.clamp(surr,1-hp.eps,1+hp.eps) * adv_b
+            a_loss = -torch.min(clip,surr).mean()
+            critic_loss = mse(td_b.detach(),new_v)
+            loss = a_loss + critic_loss
 
-            a_loss = torch.clamp(surr,1-hp.eps,1+hp.eps) * adv
-            c_loss = F.smooth_l1_loss(td_target, critic(s))
-            loss = a_loss + 0.5 * c_loss
             actor_optim.zero_grad()
-            critic_optim.zero_grad()
-            loss.mean().backward(retain_graph=True)
+            loss.backward(retain_graph=True)
             actor_optim.step()
+
+            critic_optim.zero_grad()
+            loss.backward(retain_graph=True)
             critic_optim.step()
 
+            loss_lst.append(loss.data.numpy())
+    print('\n Loss : {}'.format(np.mean(loss_lst)))
+    return np.mean(loss_lst)
 
-
-
+#TODO : checking matrix dimension
+#TODO : update gradient per model
 
 
 
