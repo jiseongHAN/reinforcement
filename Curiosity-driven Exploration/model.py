80c87b1bd5f9ad8e782b9e6eeb42ec76227f182b
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-09 17:48:41 +0900

    Update model.py

--- Curiosity-driven Exploration/model.py
+++ Curiosity-driven Exploration/model.py
@@ -24,13 +24,13 @@ class CNNActor(nn.Module):
         nn.init.xavier_uniform_(self.conv2.weight)
         nn.init.xavier_uniform_(self.conv3.weight)
 
-    def forward(self,x):
+    def forward(self,x,dim=1):
         x = swish(self.conv1(x))
         x = swish(self.conv2(x))
         x = swish(self.conv3(x))
         x = x.view(-1,18496)
         x = swish(self.fc1(x))
-        prob = F.softmax(self.pi(x))
+        prob = F.softmax(self.pi(x),dim = dim)
         return prob
 
 class CNNCritic(nn.Module):
@@ -57,9 +57,9 @@ class CNNCritic(nn.Module):
         return v
 
 
-class ICM(nn.Module):
+class ICMModel(nn.Module):
     def __init__(self,n_action):
-        super(ICM,self).__init__()
+        super(ICMModel,self).__init__()
         feature_output = 18496
         self.conv1 = nn.Conv2d(4,32,4,2)
         self.conv2 = nn.Conv2d(32,64,4,2)
@@ -69,6 +69,7 @@ class ICM(nn.Module):
         self.fc_pi = nn.Linear(cf.hidden,n_action)
         self.res = nn.Linear(n_action + cf.hidden,cf.hidden)
         self.res1 = nn.Linear(cf.hidden,cf.hidden)
+        self.n_action = n_action
 
         #### xavier init
         nn.init.xavier_uniform_(self.conv1.weight)
@@ -88,12 +89,12 @@ class ICM(nn.Module):
         return x
     def inverse(self,x):
         x = swish(self.inv(x))
-        x = self.fc_pi(x)
+        x = F.softmax(self.fc_pi(x),1)
         return x
     def residual(self,x):
         x = swish(self.res(x))
         x = self.res1(x)
-        x = list(x)
+        x = [x]
         x = x * 8
         return x
     def forward_net1(self,x):
@@ -104,8 +105,12 @@ class ICM(nn.Module):
         return self.res(x)
 
     def forward(self,inputs):
-        state, action, _, s_prime, _ = inputs
-
+        state, action, s_prime= inputs
+        action = torch.LongTensor(action)
+        action_onehot = torch.FloatTensor(
+            len(action), self.n_action)
+        action_onehot.zero_()
+        action_onehot.scatter_(1, action.view(len(action), -1), 1)
         encode_state = self.feature(state)
         encode_next_state = self.feature(s_prime)
 
@@ -114,16 +119,16 @@ class ICM(nn.Module):
         pred_action = self.inverse(pred_action)
         ######################################
         # get pred next state
-        pred_next_state_feature_orig = torch.cat((encode_state, action), 1)
+        pred_next_state_feature_orig = torch.cat((encode_state, action_onehot), 1)
         pred_next_state_feature_orig = self.forward_net1(pred_next_state_feature_orig)
 
         # residual
         for i in range(4):
-            pred_next_state_feature = self.residual(torch.cat((pred_next_state_feature_orig, action), 1))[i * 2]
+            pred_next_state_feature = self.residual(torch.cat((pred_next_state_feature_orig, action_onehot), 1))[i * 2]
             pred_next_state_feature_orig = self.residual(
-                torch.cat((pred_next_state_feature, action), 1))[i * 2 + 1] + pred_next_state_feature_orig
+                torch.cat((pred_next_state_feature, action_onehot), 1))[i * 2 + 1] + pred_next_state_feature_orig
 
-        pred_next_state_feature = self.forward_net2(torch.cat((pred_next_state_feature_orig, action), 1))
+        pred_next_state_feature = self.forward_net2(torch.cat((pred_next_state_feature_orig, action_onehot), 1))
 
         real_next_state_feature = encode_next_state
         return real_next_state_feature, pred_next_state_feature, pred_action
