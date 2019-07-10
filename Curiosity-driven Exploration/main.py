7be8da927c36e39039bd7be833a69f3ad0dfe382
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-10 15:55:31 +0900

    Update main.py

--- Curiosity-driven Exploration/main.py
+++ Curiosity-driven Exploration/main.py
@@ -1,3 +1,4 @@
+from agent import *
 from env import *
 from model import *
 from utils import *
@@ -7,39 +8,63 @@ import random
 import numpy as np
 from torch.distributions.categorical import Categorical
 from collections import deque
-from agent import *
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT, RIGHT_ONLY
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 env_id = cf.env
-env = MarioEnv(env_id, cf.stacked_frame, cf.height, cf.width, cf.skip)
-n_action = env.n_action
-Critic = CNNCritic()
-Actor = CNNActor(n_action)
-ICM = ICMModel(n_action)
-memory = deque(maxlen= 10000)
-optimizer = optim.Adam(list(Critic.parameters())+ list(Actor.parameters())+ list(ICM.parameters()),lr = cf.lr)
-train = TRAIN(memory,Actor,Critic,ICM,optimizer)
+mario = gym_super_mario_bros.make(env_id)
+env = SkippedEnv(mario, cf.skip)
+# env = JoypadSpace(env,COMPLEX_MOVEMENT)
+env = JoypadSpace(env,RIGHT_ONLY)
+n_action = env.action_space.n
+critic = CNNCritic().to(device)
+actor = CNNActor(n_action).to(device)
+icm = ICMModel(n_action).to(device)
+memory = []
+actor_optimizer = optim.Adam(actor.parameters(),lr = cf.lr)
+critic_optimizer = optim.Adam(critic.parameters(),lr = cf.lr)
+icm_optimizer = optim.Adam(icm.parameters(),lr = cf.lr)
+optimizer = optim.Adam(list(actor.parameters()) + list(critic.parameters()) + list(icm.parameters()), lr = cf.lr)
+
 ### make transition or roll-out
 for n_epi in range(cf.iter_max):
     s = env.reset()
-    done_mask = 1
+    s = prepro(s)
+    state = deque(maxlen=cf.stacked_frame)
+    for _ in range(cf.stacked_frame):
+        state.append(s)
     score = 0.0
     step = 0
-    while done_mask == 1:
-        prob = Actor(torch.tensor(s,dtype=torch.float).unsqueeze(0))
-        action = Categorical(prob)
-        s, a, r, s_prime, done_mask = env.run(action.sample().item())
+    done = False
+    while not done:
+        # env.render()
+        prob = actor(torch.tensor(state,dtype=torch.float).unsqueeze(0).to(device))
+        action = Categorical(prob.cpu())
+        a = action.sample().item()
+        s_prime, r, done, info = env.step(a)
+        s_prime = prepro(s_prime)
+        state_prime = state.copy()
+        state_prime.append(s_prime)
         score += r
-        data = (s,a,r,s_prime,done_mask)
-        s = s_prime
+        done_mask = 0 if done else 1
+        data = (state,a,r,state_prime,done_mask, prob.squeeze()[a].item())
+        state = state_prime
         memory.append(data)
         step += 1
         if done_mask == 0:
             break
-    train()
-    # TODO : train_net에 필요한 것 : intrinsic reward 구하기
+        if len(memory) == 5120:
+            train_net(memory,actor,critic,icm,optimizer)
+            memory = []
     print('#{} 총 점수 : {} 스텝 : {}'.format(n_epi,score,step))
     score = 0.0
     step = 0.0
-    train.memory = deque(maxlen=10000)
 
+# actor_optimizer,critic_optimizer,icm_optimizer
+
+## TODO : 자잘한 버그 / agent와 main 간의 호환! - env 안정성
+## TODO : GPU 버전 만들기 / + a3c
