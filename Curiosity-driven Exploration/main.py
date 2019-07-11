5a25346f447c2bc97e58be5436ab29f384a3d67f
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-11 16:58:16 +0900

    state stack method -> deque to np.array

--- Curiosity-driven Exploration/main.py
+++ Curiosity-driven Exploration/main.py
@@ -7,38 +7,37 @@ import torch
 import random
 import numpy as np
 from torch.distributions.categorical import Categorical
-from collections import deque
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
-from gym_super_mario_bros.actions import COMPLEX_MOVEMENT, RIGHT_ONLY
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT, RIGHT_ONLY, SIMPLE_MOVEMENT
+from tensorboardX import SummaryWriter
+
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
 env_id = cf.env
-mario = gym_super_mario_bros.make(env_id)
-env = SkippedEnv(mario, cf.skip)
-# env = JoypadSpace(env,COMPLEX_MOVEMENT)
-env = JoypadSpace(env,RIGHT_ONLY)
+env = env_make(env_id,SIMPLE_MOVEMENT)
 n_action = env.action_space.n
 critic = CNNCritic().to(device)
 actor = CNNActor(n_action).to(device)
 icm = ICMModel(n_action).to(device)
 memory = []
-actor_optimizer = optim.Adam(actor.parameters(),lr = cf.lr)
-critic_optimizer = optim.Adam(critic.parameters(),lr = cf.lr)
-icm_optimizer = optim.Adam(icm.parameters(),lr = cf.lr)
+actor_optimizer = optim.Adam(actor.parameters(),lr = 0.0001)
+critic_optimizer = optim.Adam(critic.parameters(),lr = 0.0002)
+icm_optimizer = optim.Adam(icm.parameters(),lr = 0.001)
 optimizer = optim.Adam(list(actor.parameters()) + list(critic.parameters()) + list(icm.parameters()), lr = cf.lr)
 
 ### make transition or roll-out
 for n_epi in range(cf.iter_max):
     s = env.reset()
     s = prepro(s)
-    state = deque(maxlen=cf.stacked_frame)
-    for _ in range(cf.stacked_frame):
-        state.append(s)
+    state = np.zeros((cf.stacked_frame,cf.height,cf.width))
+    for i in range(cf.stacked_frame):
+        state[i,::] = s
     score = 0.0
     step = 0
+    n_train = 0
     done = False
     while not done:
         # env.render()
@@ -46,25 +45,31 @@ for n_epi in range(cf.iter_max):
         action = Categorical(prob.cpu())
         a = action.sample().item()
         s_prime, r, done, info = env.step(a)
-        s_prime = prepro(s_prime)
-        state_prime = state.copy()
-        state_prime.append(s_prime)
+        state_prime = np.zeros_like(state)
+        state_prime[:3] = state[1:]
+        state_prime[3, :, :] = prepro(s_prime)
         score += r
         done_mask = 0 if done else 1
         data = (state,a,r,state_prime,done_mask, prob.squeeze()[a].item())
-        state = state_prime
+        # print('state = {}'.format((state == state_prime).all()))
+        # print('fixed = {}'.format((s == prepro(s_prime)).all()))
         memory.append(data)
+        state = state_prime
         step += 1
-        if done_mask == 0:
+        if done:
             break
-        if len(memory) == 5120:
+        if len(memory) >= 1600:
+            n_train += 1
             train_net(memory,actor,critic,icm,optimizer)
             memory = []
     print('#{} 총 점수 : {} 스텝 : {}'.format(n_epi,score,step))
+    if info['stage'] == 2:
+        print('Stage 1 Clear')
     score = 0.0
     step = 0.0
-
+env.close()
 # actor_optimizer,critic_optimizer,icm_optimizer
 
-## TODO : 자잘한 버그 / agent와 main 간의 호환! - env 안정성
-## TODO : GPU 버전 만들기 / + a3c
+## TODO : 자잘한 버그 / agent와 main 간의 호환! - env 안정성( 어느정도 클리어 )
+## TODO : GPU 버전 만들기(O) / + a3c
+## TODO : ICM 넣어서 돌려보기
