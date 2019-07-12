c5d353ecfaab72af849ccfbdbb701d4c07839ec1
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-12 16:46:20 +0900

    add hyperparameters for saving

--- PPO/hp.py
+++ PPO/hp.py
@@ -8,3 +8,9 @@ actor_lr = 0.0002
 critic_lr = 0.0002
 l2_rate = 0.001
 batch_size = 64
+
+resume = False
+actor_path = 'model/box'
+critic_path = 'model/box'
+actor_name = 'actor'
+critic_name = 'critic'
