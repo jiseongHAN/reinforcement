42a740f1e9c71ecb0b21fe5bb56f742ad30b22c5
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-16 11:14:57 +0900

    rollout length 추가

--- PPO/hp.py
+++ PPO/hp.py
@@ -2,13 +2,13 @@ hidden = 256
 gamma = 0.99
 lam = 0.98
 eps = 0.2
-epoch = 10
+epoch = 4
 iter_max = 10000
 actor_lr = 0.0002
 critic_lr = 0.0002
 l2_rate = 0.001
-batch_size = 64
-
+batch_size = 4
+rollout_len = 128
 resume = False
 actor_path = 'model/box'
 critic_path = 'model/box'
