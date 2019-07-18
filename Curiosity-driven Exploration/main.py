8dec3e7cc736d8d85fd5fbaac954ef2ad7fa5567
jiseongHAN <48741026+jiseongHAN@users.noreply.github.com>
2019-07-18 16:44:52 +0900

    Delete A3C , add nn.DataParallel for GPU

--- Curiosity-driven Exploration/main.py
+++ Curiosity-driven Exploration/main.py
@@ -1,22 +1,16 @@
-import torch.multiprocessing as mp
 from agent import *
 from env import *
 from model import *
 from utils import *
 import config as cf
 import torch
-import random
 import numpy as np
 from torch.distributions.categorical import Categorical
-import gym_super_mario_bros
-from nes_py.wrappers import JoypadSpace
-from gym_super_mario_bros.actions import COMPLEX_MOVEMENT, RIGHT_ONLY, SIMPLE_MOVEMENT
-from tensorboardX import SummaryWriter
-
+import torch.optim as optim
 
 ### make transition or roll-out
-def main(env_id,action_space,actor,critic,icm,optimizer, save,index=0):
-    env = env_make(env_id, action_space)
+def main():
+    env = env_make(cf.env,ismario=cf.mario)
     memory = []
     n_train = 0
     n_epi = 0
@@ -38,8 +32,8 @@ def main(env_id,action_space,actor,critic,icm,optimizer, save,index=0):
                 a = action.sample().item()
                 s_prime, r, done, info = env.step(a)
                 state_prime = np.zeros_like(state)
-                state_prime[:3] = state[1:]
-                state_prime[3, :, :] = prepro(s_prime)
+                state_prime[:cf.stacked_frame-1] = state[1:]
+                state_prime[cf.stacked_frame-1, :, :] = prepro(s_prime)
                 score += r
                 done_mask = 0 if done else 1
                 data = (state,a,r,state_prime,done_mask, prob.squeeze()[a].item())
@@ -50,17 +44,16 @@ def main(env_id,action_space,actor,critic,icm,optimizer, save,index=0):
                 step += 1
                 if done:
                     break
-                if len(memory) >= 3200:
-                    n_train += 1
-                    train_net(memory,actor,critic,icm,optimizer)
-                    print('Worker{} - #{} Successfully Trained'.format(index+1,n_train))
-                    memory = []
-                    if save and n_train % 100 == 0 and n_train != 0:
-                        model_save(actor, cf.actor_path, cf.actor_name)
-                        model_save(critic, cf.critic_path, cf.critic_name)
-                        model_save(icm, cf.icm_path, cf.icm_name)
-                        print('Worker{} - #{} : Successfully save model'.format(index+1,n_train))
-            print('Worker{} - #{} 총 점수 : {} 스텝 : {} 스테이지 : {}'.format(index+1, n_epi,score,step,info['stage']))
+            n_train += 1
+            train_net(memory,actor,critic,icm,optimizer)
+            print('#{} Successfully Trained'.format(n_train))
+            memory = []
+            if n_train % 500 == 0 and n_train != 0:
+                model_save(actor, cf.actor_path, cf.actor_name)
+                model_save(critic, cf.critic_path, cf.critic_name)
+                model_save(icm, cf.icm_path, cf.icm_name)
+                print('#{} : Successfully save model'.format(n_train))
+        print('#{} 총 점수 : {} 스텝 : {}'.format(n_epi,score,step))
         score = 0.0
         step = 0.0
         n_epi += 1
@@ -70,39 +63,19 @@ def main(env_id,action_space,actor,critic,icm,optimizer, save,index=0):
 
 
 if __name__ == '__main__':
-    if cf.gpu and cf.a3c:
-        raise NotImplementedError
-    if cf.gpu:
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        print('device => {}'.format(device))
 
-    else:
-        device = 'cpu'
-        print('device => {}'.format(device))
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print('device => {}'.format(device))
 
-
-    torch.manual_seed(123)
-    mp.set_start_method('spawn')
-
-
-    if cf.action_space == 0:
-        n_action = 12
-        action_space = COMPLEX_MOVEMENT
-    elif cf.action_space == 1:
-        n_action = 7
-        action_space = SIMPLE_MOVEMENT
-    else:
-        n_action = 5
-        action_space = RIGHT_ONLY
-
-
-    critic = CNNCritic().to(device)
-    actor = CNNActor(n_action).to(device)
-    icm = ICM(n_action).to(device)
+    n_action = gym.make(cf.env).action_space.n
+    critic = nn.DataParallel(CNNCritic()).to(device)
+    actor = nn.DataParallel(CNNActor(n_action)).to(device)
+    icm = nn.DataParallel(ICM(n_action)).to(device)
     actor_optimizer = optim.Adam(actor.parameters(), lr=0.0002)
     critic_optimizer = optim.Adam(critic.parameters(), lr=0.0002)
     icm_optimizer = optim.Adam(icm.parameters(), lr=0.0002)
-
+    optimizer = optim.Adam(list(actor.parameters()) + list(critic.parameters()) + list(icm.parameters()), lr=cf.lr,
+                           weight_decay=cf.l2_rate)  # weight_decay
 
     if cf.resume:
         try:
@@ -114,29 +87,7 @@ if __name__ == '__main__':
     else:
         print('Start from Scratch')
 
-    if cf.a3c:
-        critic.share_memory()
-        actor.share_memory()
-        icm.share_memory()
-    optimizer = optim.Adam(list(actor.parameters()) + list(critic.parameters()) + list(icm.parameters()), lr=cf.lr,
-                           weight_decay=cf.l2_rate)  # weight_decay
-
-    if cf.a3c:
-        processes = []
-        for index in range(cf.num_processes):
-            if index == 0:
-                process = mp.Process(target=main, args=('SuperMarioBros-1-{}-v0'.format(index+1),action_space,actor,critic,icm,optimizer, True, index))
-            else:
-                process = mp.Process(target=main,args = ('SuperMarioBros-1-{}-v0'.format(index+1),action_space,actor,critic,icm,optimizer, False, index))
-            process.start()
-            processes.append(process)
-        for process in processes:
-            process.join()
-
-        print('START!!!!!!')
-    else:
-        main(cf.env,action_space,actor,critic,icm,optimizer, False)
-
+    main()
 # 자잘한 버그 / agent와 main 간의 호환! - env 안정성(클리어)
 # TODO : hyperparameter 조정
-# TODO : GPU 버전 만들기(O) / + a3c / argparse
+# TODO : GPU 버전 만들기(O) / + a3c(O) / argparse
